a6bf771414476120b3477e52c34e2a97d729b4c6
daltonco <daltonco@mail.uc.edu>
2021-11-07 16:25:34 -0500

    Add project files.

---
+++ Shit/Controllers/NameController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shit.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Name> Get()
+        {
+            return Enumerable.Range(1, 1).Select(index => new Name
+            {
+                fName = "Colton",
+                lName = "Dalton"
+            });
+    }
+}
+}
---
+++ Shit/Name.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shit
+{
+    public class Name
+    {
+        public String fName { get; set; }
+        public String lName { get; set; }
+    }
+}
